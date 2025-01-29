518d63be1466ec059c76a86dbeca06642adf8dd2
Allan Vestergaard <info@avz.dk>
2025-01-29 14:32:13 +0100

    typing

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import os
 from sparxmodel import Sparxmodel, Object, Attribute, Package
 from sqlmodel import select
 from dotenv import load_dotenv
+from typing import List
 
 if __name__ == '__main__':
     load_dotenv()
     DATABASE_URL=os.environ.get('DATABASE_URL')
 
     model = Sparxmodel(DATABASE_URL)
-
  
-    q = select(Attribute)
-    items = model.exec(q).all()
+    q = select(Object).where(Object.name == 'FagelementansvarligEnhed')
+
+    items :List[Object] = model.exec(q).all()
 
     for item in items:
-        print(item.object_id, item.name, item.object.name)
+        print(item.object_id, item.name, item.object_type)
