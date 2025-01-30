4a20f72bab025c0bd3bce41821c984948b123663
Allan Vestergaard <info@avz.dk>
2025-01-30 10:46:13 +0100

    Fix recursive

--- main.py
+++ main.py
@@ -24,32 +24,32 @@ def dump(items):
                 attrDict[tagDict['property']]=tagDict['value']
             attrList.append(attrDict)
         objDict['attributes']=attrList
-        print(f"OBJ: {objDict}")
         model.append(objDict)
     return model
 
 
 def ObjectsInPackageRecursive(package : sparxmodel.Package) -> List[sparxmodel.Object]:
-    print(f"Package: {package.name}")   
+    """Recursively get all objects in a package not including the packages itself"""
+
     objects: List[sparxmodel.Object] =[]
     for subpackage in package.children:
-        objects=ObjectsInPackageRecursive(subpackage)
+        subobj=ObjectsInPackageRecursive(subpackage)
+        objects=objects+subobj
     for obj in package.objects:
         if obj.object_type != 'Package':
-            print(f"Object: {obj.name}")
             objects.append(obj)
     return objects
 
 if __name__ == '__main__':
     load_dotenv()
-    DATABASE_URL=os.environ.get('DATABASE_URL')
+    DATABASE_URL=os.environ.get('DATABASE_URL') # ex. mysql+pymysql://user:password@server.com/mydatabase?charset=utf8mb4
 
     model = sparxmodel.Sparxmodel(DATABASE_URL)
  
     q = select(sparxmodel.Package).where(sparxmodel.Package.name == 'SIS DEV5')
     package = model.exec(q).first()
     objects = ObjectsInPackageRecursive(package)
+    print(f"Found {len(objects)} objects in package {package.name}")
     d=dump(objects)
-    with open('output.json', 'w') as f:
-        json.dump(d, f, indent=4)
+    #with open('output.json', 'w') as f: json.dump(d, f, indent=4)
 
