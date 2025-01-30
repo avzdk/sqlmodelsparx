b641641790b24e62c2a2f379e4a080013a2d33bc
Allan Vestergaard <info@avz.dk>
2025-01-30 09:45:45 +0100

    AttributeTag

--- sparxmodel/sparxmodel.py
+++ sparxmodel/sparxmodel.py
@@ -1,26 +1,25 @@
 from sqlmodel import Session, Field, Relationship, SQLModel, create_engine, select
 
 
-
-
 class Sparxmodel():
     def __init__(self, url):
-        self.engine = create_engine(echo=True, url=url) 
+        self.engine = create_engine(url=url) 
         self.session = Session(self.engine) 
 
     def exec(self, query):
         return self.session.exec(query)
 
+
 class Object(SQLModel, table=True):
     __tablename__ = 't_object'
     object_id: int | None = Field(default=None, primary_key=True)   #None = autoincrement in database
     name: str 
-    attributes: list["Attribute"] = Relationship(back_populates="object")
-    properties: list["ObjectProperty"] = Relationship(back_populates="object")
     object_type : str 
     Note : str
     Stereotype : str
     Abstract : bool
+    attributes: list["Attribute"] = Relationship(back_populates="object")
+    properties: list["ObjectProperty"] = Relationship(back_populates="object")
 
 class Attribute(SQLModel, table=True):
     __tablename__ = 't_attribute'
@@ -28,6 +27,7 @@ class Attribute(SQLModel, table=True):
     name: str
     object_id: int = Field(foreign_key="t_object.object_id")
     object: "Object" = Relationship(back_populates="attributes")
+    tags: list["AttributeTag"] = Relationship(back_populates="attribute")
 
 class ObjectProperty(SQLModel, table=True):
     __tablename__ = 't_objectproperties'
@@ -38,6 +38,14 @@ class ObjectProperty(SQLModel, table=True):
     notes: str
     object: "Object" = Relationship(back_populates="properties")
 
+class AttributeTag(SQLModel, table=True):
+    __tablename__ = 't_attributetag'
+    propertyID: int | None = Field(default=None, primary_key=True)   #None = autoincrement in database
+    elementID: int = Field(foreign_key="t_attribute.id")
+    property: str
+    value: str
+    notes: str
+    attribute: "Attribute" = Relationship(back_populates="tags")
 
 class Package(SQLModel, table=True):
     __tablename__ = 't_package'
